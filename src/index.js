17a024fbbfeafb4898d25112c8a7c277f1cc5e17
Surya Prakash <surya.jinagam@gmail.com>
2026-06-28 22:28:30 +0530

    fix: correct import path for profileStatsModel in stats.controller.js and index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./app.js";
 import { dbConnect, db } from "./config/db/index.js";
 import profileModel  from '../src/models/profile.model.js';
-import profileStatsModel  from './models/profileStats.model.js';
+import profileStatsModel  from '../src/models/profileStats.model.js';
 
 const startServer = async () => {
   try {
